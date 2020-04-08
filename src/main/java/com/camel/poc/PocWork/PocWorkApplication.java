14d4c68dba3d830423529cd2fd22dac8c5d5eef8
veerendranadhrgmail.com <veerendranadhrgmail.com@veerendnadhsmbp>
2020-04-08 21:54:50 +0530

    App Test commit

---
+++ src/main/java/com/camel/poc/PocWork/PocWorkApplication.java
@@ -0,0 +1,13 @@
+package com.camel.poc.PocWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocWorkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/model/Employee.java
@@ -0,0 +1,21 @@
+package com.model;
+
+public class Employee {
+
+	
+	private int eId;
+	private String eName;
+	public int geteId() {
+		return eId;
+	}
+	public void seteId(int eId) {
+		this.eId = eId;
+	}
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	
+}
